3e7dc9108e475a68c3918607ddddeecff9c323ae
Di Battista Alessio <alessio.dibattista@cz.lan>
2025-11-07 13:22:00 +0100

    inizio della creazione per la classe per la lettura convertitore.Lettore con controllo del nome del file per la lettura prima di farlo

--- tokenizer/src/Main.java
+++ tokenizer/src/Main.java
@@ -3,6 +3,7 @@ import convertitore.*;
 public class Main {
     /*
     TOKENIZER DI ANTIPYTHON
+    todo CLASSE MAIN PRESENTE SOLO PER TESTARE LE CLASSI POI VERRA ELIMINATA
      */
     public static void main(String[] args) {
 
--- tokenizer/src/convertitore/Lettore.java
+++ tokenizer/src/convertitore/Lettore.java
@@ -1,5 +1,5 @@
 package convertitore;
-import java.io.*;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -11,7 +11,6 @@ public class Lettore {
     private ArrayList<String> lineeClodice;
 
     /**
-     *
      * @param nomeFile il nome del file deve contenere l'estensione e non tutto il percorso del file perché
      *                 poi verrà controllato il nome e aggiunto src/scriptCodici/ prima del nome per avere il percorso
      *                 giusto
@@ -21,10 +20,34 @@ public class Lettore {
         this.lineeClodice = new ArrayList<>();
     }
 
+    public String run() {
+        //todo
+        return null;
+    }
+
     /**
+     * Il metodo controlla se l,estensione è corretta, ovvero .unpy salvato nell'attributo estensione:String,
+     * nel caso lo fosse, sistema il nome e aggiunge il pezzo del percorso src/scriptCodici/ in modo che sia leggibile
+     *
      * @param nome inserito il nome del file in questione che va controllato se ha l'estensione giusta
      */
     private void sistemaNomeFile(String nome) {
+        //todo se cercaEstensione non trova l'estensione la aggiunge invece se ne trova una diversa scatta un'eccezione
+
+        //todo se controllaPathNomeFile trova una path prima del nome del file la rimuove
+    }
+
+    /**
+     * TODO SPIEGAZIONE METODO COME FUNZIONA E ALTRO
+     * @return
+     */
+    private void controllaEstensione() {
+        //todo cercare se la sottostringa estensione si trova dentro al nome del file
+        
+        //todo oltre ad aver controllato bisogna anche aggiungere l'estensione se manca
+    }
+
+    private boolean controllaPathNomeFile() {
 
     }
 }
