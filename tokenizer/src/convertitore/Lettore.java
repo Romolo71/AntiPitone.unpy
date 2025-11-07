279ff44b65d8b29835222d6bae2809c464a65b7d
Di Battista Alessio <alessio.dibattista@cz.lan>
2025-11-07 13:01:11 +0100

    creazione progetto del tokenizer

--- tokenizer/src/convertitore/Lettore.java
+++ tokenizer/src/convertitore/Lettore.java
@@ -1,5 +1,6 @@
 package convertitore;
 import java.io.*;
+import java.util.ArrayList;
 
 /**
  * Classe per leggere il file e restituire un'array di stringhe da poi convertire in token
@@ -7,9 +8,23 @@ import java.io.*;
 public class Lettore {
     private String nomeProgramma;
     private final String estensione = ".unpy";
-    private String[] lineeClodice;
+    private ArrayList<String> lineeClodice;
 
+    /**
+     *
+     * @param nomeFile il nome del file deve contenere l'estensione e non tutto il percorso del file perché
+     *                 poi verrà controllato il nome e aggiunto src/scriptCodici/ prima del nome per avere il percorso
+     *                 giusto
+     */
     public Lettore(String nomeFile) {
-        
+        this.nomeProgramma = nomeFile;
+        this.lineeClodice = new ArrayList<>();
+    }
+
+    /**
+     * @param nome inserito il nome del file in questione che va controllato se ha l'estensione giusta
+     */
+    private void sistemaNomeFile(String nome) {
+
     }
 }
