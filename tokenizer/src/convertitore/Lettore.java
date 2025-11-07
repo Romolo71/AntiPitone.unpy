8822b64f502d1ca835434abeac761b40ec27d192
Di Battista Alessio <alessio.dibattista@cz.lan>
2025-11-07 12:54:54 +0100

    creazione progetto del tokenizer

---
+++ tokenizer/src/convertitore/Lettore.java
@@ -0,0 +1,15 @@
+package convertitore;
+import java.io.*;
+
+/**
+ * Classe per leggere il file e restituire un'array di stringhe da poi convertire in token
+ */
+public class Lettore {
+    private String nomeProgramma;
+    private final String estensione = ".unpy";
+    private String[] lineeClodice;
+
+    public Lettore(String nomeFile) {
+        
+    }
+}
